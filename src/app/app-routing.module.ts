d229e68febc05b5ed483d65311c284b1cab7cbe9
adriana cortes <adrianm-cortesv@unilibre.edu.co>
2019-11-10 19:20:09 -0500

    agregar login and registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent } from './components/home/home.component';
 import {SolitudsalaComponent} from './components/solitudsala/solitudsala.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'solitudsala', component: SolitudsalaComponent},
   {path: '*', redirectTo: ''}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,13 +12,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatSelectModule} from '@angular/material/select'
 import {MatButtonModule} from '@angular/material/button';
-
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import {
+  MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
+  MatToolbarModule, MatMenuModule,MatIconModule, MatProgressSpinnerModule
+} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
     SolitudsalaComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,8 +34,13 @@ import {MatButtonModule} from '@angular/material/button';
     ReactiveFormsModule,
     MatSlideToggleModule,
     MatSelectModule,
-    MatButtonModule
-  ],
+    MatButtonModule,
+    BrowserAnimationsModule,
+    
+    
+    
+    ],
+    
   providers: [    DynamicFormBuilder],
   bootstrap: [AppComponent]
 })
